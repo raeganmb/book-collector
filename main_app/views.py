d370474ccee717a1e44af2a614f7908d86828202
raeganmb <raeganboucher2@gmail.com>
2022-11-07 20:42:57 -0400

    Implement first model

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .models import Book
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def books_index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', { 'books': books })
