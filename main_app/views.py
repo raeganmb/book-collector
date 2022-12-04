e61126c9b1bab361176ffaf33a3f85b825602ea0
raeganmb <raeganboucher2@gmail.com>
2022-12-04 15:26:16 -0400

    urls, views, templates, models added

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
 from .models import Book
 
 # Create your views here.
@@ -15,3 +12,7 @@ def about(request):
 def books_index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', { 'books': books })
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', { 'book': book })
